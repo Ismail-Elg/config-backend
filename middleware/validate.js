d8b6350ff48d45f1c40ddff777bf52b23747ec92
ismailElg <91196906+Ismail-Elg@users.noreply.github.com>
2022-12-18 12:56:45 +0100

    feat: added privatekey

--- middleware/validate.js
+++ middleware/validate.js
@@ -2,6 +2,7 @@ let express = require("express");
 let router = express.Router();
 
 const jwt = require('jsonwebtoken');
+const secretOrPrivateKey = 'GciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzd';
 
 router.post('/', function (req, res, next) {
     jwt.verify(req.body.token, secretOrPrivateKey, function (err, decoded) {
